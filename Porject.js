c5e2740306e8b30e77af803ee12cb7d0541b01e2
DominikCarreon <dvcarreon@student.apc.edu.ph>
2026-01-05 22:52:20 +0800

    Added Midterm Project

---
+++ Porject.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+
